36a3fa605251d185aaa8ecb049f57a21f19c0925
vachagunn <vachikpuper@gmail.com>
2023-01-19 00:12:41 +0500

    Merge в main (1.0.0)

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -2,16 +2,14 @@ import { NavLink } from 'react-router-dom';
 
 import './style.css'
 
-import { categories } from '../../helpers/categoriesList';
 import { useParams } from 'react-router-dom';
+import CategoriesList from '../../pages/CategoriesList';
 
 const Category = (props) => {
-    const {id} = useParams();
-    const category = categories[id];
 
-    return ( 
+    return (
         <li className="categories-list-item">
-            <NavLink to="/category" className="categories-list-item-link">
+            <NavLink to='/' className="categories-list-item-link">
                 <img src={props.image} width="270" height="170" alt={props.title}/>
                 <p className="categories-list-item-title">{props.title}</p>
             </NavLink>
