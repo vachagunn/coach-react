50489b48001ff905ef3984933c87793c42b85d4a
vachagunn <vachikpuper@gmail.com>
2023-01-25 19:36:56 +0500

    Удаление ненужного файла + проверка работы

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -2,18 +2,14 @@ import { NavLink } from 'react-router-dom';
 
 import './style.css'
 
-import { useParams } from 'react-router-dom';
-import CategoriesList from '../../pages/CategoriesList';
-
 const Category = (props) => {
-
     return (
-        <li className="categories-list-item">
-            <NavLink to='/' className="categories-list-item-link">
-                <img src={props.image} width="270" height="170" alt={props.title}/>
-                <p className="categories-list-item-title">{props.title}</p>
-            </NavLink>
-        </li>   
+            <li className="categories-list-item">
+                <NavLink to={`/categories_list/${props.id}`} className="categories-list-item-link">
+                    <img src={props.image} width="270" height="170" alt={props.title}/>
+                    <p className="categories-list-item-title">{props.title}</p>
+                </NavLink>
+            </li>
      );
 }
  
